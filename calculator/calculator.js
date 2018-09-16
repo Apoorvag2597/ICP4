cecf042022eb2defaf36afb1319a4049fcba4bde
Apoorvag2597 <42699442+Apoorvag2597@users.noreply.github.com>
2018-09-15 21:10:58 -0500

    icp4

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,21 +3,21 @@ app.controller('calcController', function ($scope) {
     var x;
     $scope.calculate = function () {
 
-        if ($scope.operator == '+') {
+        if ($scope.operator == '+') { // addition operation
 
             x = parseInt($scope.a) + parseInt($scope.b);
         }
-        if ($scope.operator == '-') {
+        if ($scope.operator == '-') { // subtraction operation
             x = $scope.a - $scope.b;
         }
-        if ($scope.operator == '*') {
+        if ($scope.operator == '*') { // multiplication operation
             x = $scope.a * $scope.b;
         }
-        if ($scope.operator == '/') {
+        if ($scope.operator == '/') { //divide oprration
             x = $scope.a / $scope.b;
         }
-        if ($scope.operator == '%') {
-            x = $scope.a / $scope.b;
+        if ($scope.operator == '%') { //modulus operation
+            x= $scope.a % $scope.b;
         }
 
         $scope.y = x;
--- todo/todo.js
+++ todo/todo.js
@@ -1,17 +1,17 @@
 var angularTodo = angular.module('angularTodo', []);
-
+// controller
 angularTodo.controller('angularTodoC', ['$scope', function ($scope) {
     // define list of items
-    $scope.items = [" ", "  "];
+    $scope.items = ["Visit India", "Visit Paris"];
 
     // Write code to push new item
-    $scope.submitNewItem = function () {
+    $scope.submitNewItem = function () {  //function for add button is created
         console.log("submitNewItem")
         $scope.items.push($scope.todoitem);
     };
 
     // Write code to complete item
-    $scope.completeItem = function (index,event) {
+    $scope.completeItem = function (index,event) { //function for complete button is created
         console.log(event);
         event.currentTarget.parentElement.firstElementChild.classList.add("text-strike")
 
@@ -19,14 +19,12 @@ angularTodo.controller('angularTodoC', ['$scope', function ($scope) {
 
     // Write code to delete item
 
-    $scope.deleteItem = function (index) {
+    $scope.deleteItem = function (index) { //function for delete button is created
+
+
         console.log(index)
+
         $scope.items.splice(index)
 
     };
 }]);
-
-
-
-
-
